3498e3f181ca1ff61340304debd591d6d7a341f2
Michal Dadej <michaldadej@aptitudesoftware.com>
2018-12-18 14:53:13 +0100

    Hot fix.

--- E2E/specs/example.spec.js
+++ E2E/specs/example.spec.js
@@ -1,16 +1,14 @@
 describe("test set", () => {
+  const url = "http://10.0.75.1:9000/#/";
   it("should open website", () => {
-    browser.get("http://10.0.75.1:9000/#/");
-    const el1 = element(by.css(".new-todo"));
+    browser.get(url);
+    const toDoInput = element(by.css(".new-todo"));
     const header = element(by.css("h1"));
     const headerText = header.getText();
     expect("Todos").toEqual(headerText);
-    el1.sendKeys("Test", protractor.Key.ENTER);
-    browser.sleep(1000);
-    el1.clear();
-    el1.sendKeys("Test2", protractor.Key.ENTER);
-    browser.sleep(1000);
-    el1.clear();
-    browser.sleep(1000);
+    toDoInput.sendKeys("Test", protractor.Key.ENTER);
+    toDoInput.clear();
+    toDoInput.sendKeys("Test2", protractor.Key.ENTER);
+    toDoInput.clear();
   });
 });
