4f2bf2772bbc33b320429177c6576d6c302efedc
kaja jablonska <kaja.jablonska@aptitudesoftware.com>
2018-12-18 15:08:09 +0100

    first test changed

--- E2E/specs/example.spec.js
+++ E2E/specs/example.spec.js
@@ -5,18 +5,9 @@ if (env === "local") {
 }
 
 describe("opisu zestawu assercji", () => {
-  // beforeEach(() =>{});
-  // beforeAll(() =>{});
-  // afterEach(() =>{});
-  // afterAll(() =>{});
-
-  // it("should open website", () => {})
-  // it("should open website", () => {})
-  // it("should open website", () => {})
   it("should open website", () => {
     browser.get(url);
     const header = browser.element(by.css("h1"));
     expect(header.getText()).toEqual("Todos");
-    // expect(element(by.css("header")).getText()).toEqual("Todos");
   });
 });
