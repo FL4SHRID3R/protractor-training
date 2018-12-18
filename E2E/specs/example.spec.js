89c21c64cf5018454121ab5154ac502f197c3e29
Michal Dadej <michaldadej@aptitudesoftware.com>
2018-12-18 14:56:24 +0100

    Add waitForAngular.

--- E2E/specs/example.spec.js
+++ E2E/specs/example.spec.js
@@ -2,13 +2,16 @@ describe("test set", () => {
   const url = "http://10.0.75.1:9000/#/";
   it("should open website", () => {
     browser.get(url);
+    browser.waitForAngular();
     const toDoInput = element(by.css(".new-todo"));
     const header = element(by.css("h1"));
     const headerText = header.getText();
     expect("Todos").toEqual(headerText);
     toDoInput.sendKeys("Test", protractor.Key.ENTER);
+    browser.waitForAngular();
     toDoInput.clear();
     toDoInput.sendKeys("Test2", protractor.Key.ENTER);
+    browser.waitForAngular();
     toDoInput.clear();
   });
 });
